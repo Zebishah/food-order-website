ae9942e9ad2b855c95f1533965dd7428511381c7
Zebishah <zebihaider123@gmail.com>
2024-07-29 21:39:28 +0500

    changes

---
+++ backend/index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+dotenv.config();
+import mongoose from "mongoose";
+import cors from "cors";
+
+const DB = process.env.MONGOURI;
+const app = express();
+
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+
+const port = process.env.PORT || 5000;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB connection successful");
+  })
+  .catch((err) => {
+    console.error("DB connection error:", err);
+  });
+
+// app.use("/Admin", AdminRoutes);
+
+app.get("/", (req, res) => {
+  res.json({ message: "hey brother" });
+});
+
+server.listen(port, () => {
+  console.log(`Server is Listening at Port ${port}`);
+});
+
+export default app;
